5faffb599b22543d5d607e181872260c1df5878b
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-02-15 19:09:45 +0700

    Values and Variables

--- Fundamental/part1/linkingFile.js
+++ Fundamental/part1/linkingFile.js
@@ -2,3 +2,17 @@ let js = 'amazing';
 if (js == 'amazing') alert('js is FUN'); //alert box
 
 console.log(1 + 2 + 40 - 100); //in console
+
+let firstname = 'Jotaro'; //declaring variable
+console.log(firstname);
+
+//let 1year = 1; syntax error cant put number first when declaring var
+//let new = 'yeah'; cant use reserved keyword for var
+let $new = 'yeah'; // or use '_' symbol (not recomended using reserved keyword)
+console.log($new);
+
+//let Person = 'DIO'; use uppercase for spesific usecase (use lowercase instead)
+
+let job1 = 'programmer'
+let myFirstJob = 'programmer' //this is much more descriptive than above
+
