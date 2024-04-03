3c9b9ee883eba6d66b7c1db9e331708a19d90eef
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-04-03 16:32:02 +0700

    If Else Statements

---
+++ Fundamental/part1/ifElseStatements.js
@@ -0,0 +1,9 @@
+let sarahAge = 15
+
+if (sarahAge >= 18){ 
+    console.log(`sarah can start driving`)
+} else{
+    ageRequire = 18 - sarahAge
+    console.log(`sarah can't start driving. 
+wait for ${ageRequire} years `) //use $ to insert value of variable inside string
+}
