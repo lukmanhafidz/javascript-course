a30f83c95c4532b1f140950b63243a8f3d28b028
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-04-03 16:37:24 +0700

    add comment in if else file

--- Fundamental/part1/ifElseStatements.js
+++ Fundamental/part1/ifElseStatements.js
@@ -5,5 +5,5 @@ if (sarahAge >= 18){
 } else{
     ageRequire = 18 - sarahAge
     console.log(`sarah can't start driving. 
-wait for ${ageRequire} years `) //use $ to insert value of variable inside string
-}
+wait for ${ageRequire} years `) //use $ to insert value of variable inside string(template literal")
+}// use ` notation if you want to use template literal
