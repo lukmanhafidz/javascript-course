a7cd9a04d145b7d439a038881e7e2c539d1c1b50
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-04-03 11:48:06 +0700

    Let Const Var

---
+++ Fundamental/part1/letConstVar.js
@@ -0,0 +1,11 @@
+let letVariable = "this is let"; //can reassign value
+console.log(letVariable); //block scope
+
+const constVariable = "this is const"; //fixed value
+console.log(constVariable);
+
+var variable = "this is var";//not recomended to use var
+console.log(variable);//function scope
+
+undeclared = "undeclared";
+console.log(undeclared);
