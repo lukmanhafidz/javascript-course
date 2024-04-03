7204fef44fe7d654f9fbd7640543cc0091d6b05a
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-04-03 11:00:21 +0700

    Data Types

---
+++ Fundamental/part1/dataTypes.js
@@ -0,0 +1,13 @@
+let testVariable = true;
+console.log(typeof testVariable) //boolean
+
+testVariable = "value";
+console.log(typeof testVariable) //string
+
+testVariable = 100;
+console.log(typeof testVariable) //number
+
+let variable
+console.log(typeof variable) //undefined
+
+let
