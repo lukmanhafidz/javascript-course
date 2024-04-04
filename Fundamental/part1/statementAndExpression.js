4a2a76586485f5dba4fdc9ec9e8725ebdc29b450
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-04-04 16:31:26 +0700

    Statements and Expressions

---
+++ Fundamental/part1/statementAndExpression.js
@@ -0,0 +1,15 @@
+// Statements and Expressions
+
+//Expressions produce value
+//Statements is processing expression to make action
+
+3 + 4 //Expressions
+1991 // Expressions
+true && false && !false // Expressions
+
+if (23 > 10) { //Statements (Expressions)
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas'; // Expressions
+console.log(`I'm ${2037 - 1991} years old ${me}`); //expecting expression inside bracket
