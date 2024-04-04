95fc9de0c607de45722f78e38340e2f4a68829d7
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-04-04 11:08:28 +0700

    Truthy and Falsy

---
+++ Fundamental/part1/truthyAndFalsy.js
@@ -0,0 +1,8 @@
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0)); //zero value
+console.log(Boolean(undefined));
+console.log(Boolean('')); //empty string
+console.log(Boolean(null));
+console.log(Boolean(NaN)); //not a number
+
+//will printed "false" boolean value
