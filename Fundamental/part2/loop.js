52491792e7484f67ff9ef08efa879fd8e0efce8c
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-12 11:37:20 +0700

    The while Loop

--- Fundamental/part2/loop.js
+++ Fundamental/part2/loop.js
@@ -82,3 +82,22 @@ const jonasBackward = [
       console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
     }
   }
+
+  // The while Loop
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+  }
+  
+  let rep = 1;
+  while (rep <= 10) {
+    // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+    rep++;
+  }
+  
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  
+  while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
+  }
