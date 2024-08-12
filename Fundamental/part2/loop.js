702481f54c5e91f75c209e7da7aa2b346e7c82c2
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-12 11:30:28 +0700

    For Loop

---
+++ Fundamental/part2/loop.js
@@ -0,0 +1,7 @@
+///////////////////////////////////////
+// Iteration: The for Loop
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 30; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+  }
