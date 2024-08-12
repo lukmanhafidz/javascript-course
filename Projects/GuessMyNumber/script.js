d42790098096c64b8cef11235b99eb49a0842d06
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-12 17:10:08 +0700

    DOM Introduction

---
+++ Projects/GuessMyNumber/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log(document.querySelector(".message").textContent); //DOM example in javascript
+// Select the first element in the document with the class name 'message'.
+// Access the text content of this element.
+// Log this text content to the console.
