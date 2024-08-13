c5e8446d51fbd8eb21fd25ae814cbbc48ec06e54
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-13 10:59:01 +0700

    DOM Manipulation

--- Projects/GuessMyNumber/script.js
+++ Projects/GuessMyNumber/script.js
@@ -4,3 +4,7 @@ console.log(document.querySelector(".message").textContent); //DOM example in ja
 // Select the first element in the document with the class name 'message'.
 // Access the text content of this element.
 // Log this text content to the console.
+
+document.querySelector(".score").textContent = 10; //DOM Manipulation
+document.querySelector(".number").textContent = 30;
+document.querySelector(".guess").value = 23;
