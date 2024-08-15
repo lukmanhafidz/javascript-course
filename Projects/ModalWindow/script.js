1d200011416ecca01d47eade92ae7993f7019806
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-15 14:22:39 +0700

    add close modal function

--- Projects/ModalWindow/script.js
+++ Projects/ModalWindow/script.js
@@ -1,14 +1,23 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const showModalBtns = document.querySelectorAll('.show-modal');
 const overlay = document.querySelector('.overlay');
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
 
 function showModal() {
   overlay.classList.remove('hidden');
   modal.classList.remove('hidden');
 }
 
+function closeModal() {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+}
+
 for (const btn of showModalBtns) {
   btn.addEventListener('click', showModal);
 }
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
