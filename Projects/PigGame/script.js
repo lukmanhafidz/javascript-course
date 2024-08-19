c3ea29a8223347e9db2e902190ff5e3c0bf550bf
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-19 12:08:48 +0700

    implement changing active player with it's current score

--- Projects/PigGame/script.js
+++ Projects/PigGame/script.js
@@ -1,18 +1,32 @@
 'use strict';
 
-const totalScoreP1 = document.getElementById('score--0');
-const currentScoreP1 = document.querySelector('current--0');
-
-const totalScoreP2 = document.getElementById('score--1');
-const currentScoreP2 = document.querySelector('current--0');
+const player1 = document.querySelector('.player--1');
+const player2 = document.querySelector('.player--2');
 
 const dice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 
 dice.style.display = 'none';
+let activePlayer = 1;
+let currentScore = 0;
 
 btnRoll.addEventListener('click', function () {
-  dice.style.display = 'block';
   let rollRand = Math.trunc(Math.random() * 6) + 1;
   dice.src = `dice-${rollRand}.png`;
+  dice.style.display = 'block';
+
+  currentScore += rollRand;
+
+  if (rollRand === 1) {
+    // document.querySelector(`score--${activePlayer}`).textContent = score;
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 1 ? 2 : 1;
+
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  }
 });
