c0b43169caee796b84974444ce5e0553187610c1
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-19 12:55:29 +0700

    implement  winner function with hold button

--- Projects/PigGame/script.js
+++ Projects/PigGame/script.js
@@ -5,10 +5,12 @@ const player2 = document.querySelector('.player--2');
 
 const dice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 dice.style.display = 'none';
 let activePlayer = 1;
 let currentScore = 0;
+let totalScore = [0, 0];
 
 btnRoll.addEventListener('click', function () {
   let rollRand = Math.trunc(Math.random() * 6) + 1;
@@ -19,9 +21,6 @@ btnRoll.addEventListener('click', function () {
   currentScore += rollRand;
 
   if (rollRand === 1) {
-    // document.querySelector(`score--${activePlayer}`).textContent = score;
-    currentScore = 0;
-
     activePlayer = swtichPlayer(activePlayer);
   } else {
     document.getElementById(`current--${activePlayer}`).textContent =
@@ -29,7 +28,26 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
+btnHold.addEventListener('click', function () {
+  let playerIndex = activePlayer - 1;
+  totalScore[playerIndex] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    totalScore[playerIndex];
+
+  if (totalScore[playerIndex] >= 10) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    activePlayer = swtichPlayer(activePlayer);
+  }
+});
+
 function swtichPlayer(activePlayer) {
+  currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent = 0;
 
   activePlayer = activePlayer === 1 ? 2 : 1;
