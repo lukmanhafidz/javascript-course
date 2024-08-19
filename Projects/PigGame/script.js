219db191443fb422cfb6ace2dc6b4fc033af1fc9
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-19 14:28:35 +0700

    implement disable button when game over

--- Projects/PigGame/script.js
+++ Projects/PigGame/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//init variable
 const player1 = document.querySelector('.player--1');
 const player2 = document.querySelector('.player--2');
 
@@ -7,27 +7,30 @@ const dice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-dice.style.display = 'none';
 let activePlayer = 1;
 let currentScore = 0;
 let totalScore = [0, 0];
+let gameOver = true;
 
+dice.style.display = 'none'; //hide dice img
+
+//Roll Dice Function
 btnRoll.addEventListener('click', function () {
   let rollRand = Math.trunc(Math.random() * 6) + 1;
 
   dice.src = `dice-${rollRand}.png`;
   dice.style.display = 'block';
 
-  currentScore += rollRand;
-
   if (rollRand === 1) {
     activePlayer = swtichPlayer(activePlayer);
   } else {
+    currentScore += rollRand; //calculate score per roll if not 1
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   }
 });
 
+//Hold Score Function
 btnHold.addEventListener('click', function () {
   let playerIndex = activePlayer - 1;
   totalScore[playerIndex] += currentScore;
@@ -35,12 +38,15 @@ btnHold.addEventListener('click', function () {
     totalScore[playerIndex];
 
   if (totalScore[playerIndex] >= 10) {
+    //check if score above max then win
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.add('player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+    gameOver = true;
+    isGameOver(gameOver);
   } else {
     activePlayer = swtichPlayer(activePlayer);
   }
@@ -50,10 +56,16 @@ function swtichPlayer(activePlayer) {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent = 0;
 
-  activePlayer = activePlayer === 1 ? 2 : 1;
+  activePlayer = activePlayer === 1 ? 2 : 1; //switching current player number
 
   player1.classList.toggle('player--active');
   player2.classList.toggle('player--active');
 
   return activePlayer;
+}
+
+function isGameOver(status) {
+  //button status when game over
+  btnRoll.disabled = status;
+  btnHold.disabled = status;
 }
