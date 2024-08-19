f7fa1a64f846565b13e91f5b4f881b9eda38ae5e
lukmanhafidz <lukmanhafidz40@gmail.com>
2024-08-19 12:30:06 +0700

    cleaning code

--- Projects/PigGame/script.js
+++ Projects/PigGame/script.js
@@ -12,6 +12,7 @@ let currentScore = 0;
 
 btnRoll.addEventListener('click', function () {
   let rollRand = Math.trunc(Math.random() * 6) + 1;
+
   dice.src = `dice-${rollRand}.png`;
   dice.style.display = 'block';
 
@@ -20,13 +21,21 @@ btnRoll.addEventListener('click', function () {
   if (rollRand === 1) {
     // document.querySelector(`score--${activePlayer}`).textContent = score;
     currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 1 ? 2 : 1;
 
-    player1.classList.toggle('player--active');
-    player2.classList.toggle('player--active');
+    activePlayer = swtichPlayer(activePlayer);
   } else {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   }
 });
+
+function swtichPlayer(activePlayer) {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+  activePlayer = activePlayer === 1 ? 2 : 1;
+
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+
+  return activePlayer;
+}
