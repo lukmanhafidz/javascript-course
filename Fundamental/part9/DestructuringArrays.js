95dc9803aa28f41a6601519eec340ef9f38c67fd
lukmanhafidz <lukmanhafidz40@gmail.com>
2025-09-23 11:39:42 +0700

    update output comment

--- Fundamental/part9/DestructuringArrays.js
+++ Fundamental/part9/DestructuringArrays.js
@@ -1,30 +1,9 @@
 'use strict';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
-
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  // ES6 enhanced object literals
-  openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -33,16 +12,16 @@ const restaurant = {
 
 
 const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
+const a = arr[0]; 
+const b = arr[1]; 
 const c = arr[2];
 
 const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+console.log(x, y, z); //output: 2 3 4
+console.log(arr); //output [2, 3, 4]
 
 let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+console.log(main, secondary); //output: Italian Vegetarian
 
 // Switching variables
 // const temp = main;
@@ -51,18 +30,18 @@ console.log(main, secondary);
 // console.log(main, secondary);
 
 [main, secondary] = [secondary, main];
-console.log(main, secondary);
+console.log(main, secondary); //output: Vegetarian Italian
 
 // Receive 2 return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+console.log(starter, mainCourse); //output: Garlic Bread Pizza
 
 // Nested destructuring
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 const [i, , [j, k]] = nested;
-console.log(i, j, k);
+console.log(i, j, k); //output: 2 5 6
 
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+console.log(p, q, r); //output: 8 9 1
