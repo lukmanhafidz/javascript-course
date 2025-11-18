1c53134346424cf8a1f12288b6c7e3cfdbca77b6
lukmanhafidz <lukmanhafidz40@gmail.com>
2025-11-18 14:49:58 +0700

    The Nullish Coalescing Operator

---
+++ Fundamental/part9/theNullishCoalescingOperator.js
@@ -0,0 +1,15 @@
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
+
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
